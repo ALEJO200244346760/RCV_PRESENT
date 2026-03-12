59382c3688249ccb8c2e93349e8a05b3090c83be
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2026-03-12 16:01:40 -0300

    cuarto

---
+++ rcv_Backend/src/main/java/com/backend/rcv/controller/PacienteCircuitoController.java
@@ -0,0 +1,27 @@
+package com.backend.rcv.controller;
+
+import com.backend.rcv.model.PacienteCircuito;
+import com.backend.rcv.repository.PacienteCircuitoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/circuito")
+@CrossOrigin(origins = "*")
+public class PacienteCircuitoController {
+
+    @Autowired
+    private PacienteCircuitoRepository repository;
+
+    @PostMapping("/recibir")
+    public ResponseEntity<?> recibirDatos(@RequestBody PacienteCircuito datos) {
+        try {
+            PacienteCircuito guardado = repository.save(datos);
+            return new ResponseEntity<>(guardado, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error al guardar: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ rcv_Backend/src/main/java/com/backend/rcv/model/PacienteCircuito.java
@@ -0,0 +1,77 @@
+package com.backend.rcv.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PacienteCircuito {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    private PatientInfo patientInfo;
+
+    @Embedded
+    private ClinicalHistory clinicalHistory;
+
+    @Embedded
+    private VitalsAndLabs vitalsAndLabs;
+
+    @ElementCollection
+    private List<Attachment> attachments;
+
+    @Embeddable
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PatientInfo {
+        private String document;
+        private String birthdate;
+        private String gender;
+        private String phone;
+        private String name;
+    }
+
+    @Embeddable
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ClinicalHistory {
+        private boolean isHypertensive;
+        private boolean isDiabetic;
+        private boolean hasDyslipidemia;
+        private boolean isSmoker;
+    }
+
+    @Embeddable
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class VitalsAndLabs {
+        private String bloodPressure;
+        private Integer totalCholesterol;
+        private Double weightKg;
+        private Integer heightCm;
+        private Integer waistCircumferenceCm;
+        private Integer estimatedGfr;
+    }
+
+    @Embeddable
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Attachment {
+        private String type;
+        private String issueDate;
+        private String fileUrl;
+    }
+}
